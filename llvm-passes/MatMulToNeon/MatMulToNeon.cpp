1c9ac9aeed545bbf0e7956caeb11818a5751b0d3
riptio777 <115140492+riptio777@users.noreply.github.com>
2025-08-20 06:52:35 +0800

    Register pass as dynamic plugin

--- llvm-passes/MatMulToNeon/MatMulToNeon.cpp
+++ llvm-passes/MatMulToNeon/MatMulToNeon.cpp
@@ -1,3 +1,5 @@
+#include "llvm/ADT/StringRef.h"
+#include "llvm/Config/llvm-config.h"
 #include "llvm/IR/Analysis.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/IRBuilder.h"
@@ -5,12 +7,16 @@
 #include "llvm/IR/PatternMatch.h"
 #include "llvm/IR/IntrinsicsAArch64.h"
 #include "llvm/Pass.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/Compiler.h"
 
 using namespace llvm;
 using namespace llvm::PatternMatch;
 
 namespace {
-class MatMulToNeanPass : public PassInfoMixin<MatMulToNeanPass> {
+class MatMulToNeonPass : public PassInfoMixin<MatMulToNeonPass> {
+public:
     PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM) {
         errs() << "Running MatMulToNeanPass" << "\n";
 
@@ -19,3 +25,25 @@ class MatMulToNeanPass : public PassInfoMixin<MatMulToNeanPass> {
 };
 
 } // end of anonymous namespace
+
+llvm::PassPluginLibraryInfo getMatMulToNeonPassPluginInfo() {
+    return {LLVM_PLUGIN_API_VERSION, "MatMulToNeonPass",
+            LLVM_VERSION_STRING,
+            [] (PassBuilder &PB) {
+                PB.registerPipelineParsingCallback(
+                    [](StringRef Name, FunctionPassManager &FPM,
+                        ArrayRef<PassBuilder::PipelineElement>) {
+                            if (Name == "matmul2neon") {
+                                FPM.addPass(MatMulToNeonPass());
+                                return true;
+                            }
+                            return false;
+                    });
+                
+            }};
+}
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+    return getMatMulToNeonPassPluginInfo();
+}
